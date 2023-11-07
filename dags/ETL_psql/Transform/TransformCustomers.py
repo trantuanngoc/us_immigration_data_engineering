6a5ff97b9b97236f43dd9039216b4dce7ddac83a
Tuan-Ngoc Tran <ngocthoson1@gmail.com>
2023-11-07 22:01:06 +0700

    edit

--- dags/ETL_psql/Transform/TransformCustomers.py
+++
@@ -1,8 +0,0 @@
-from ETL_psql.Transform.Transform import Transform_df
-
-class Transform_customer_df:
-    def transform(self):
-        self.df.drop(columns=['City', 'State', 'Country'], inplace=True);
-
-
-
