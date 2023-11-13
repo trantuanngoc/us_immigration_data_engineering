9e3d9b19f712dc21685d2d282c63c615518a22a4
Tuan-Ngoc Tran <ngocthoson1@gmail.com>
2023-11-13 11:53:46 +0700

    edit

--- dags/scripts/spark/data_transformation/country_transformation.py
+++ dags/scripts/spark/data_transformation/country_transformation.py
@@ -1,9 +1,19 @@
+from pyspark.sql.functions import monotonically_increasing_id
+from pyspark.sql.functions import col, round, mean
+
+
 class CountryTransformer:
     def __init__(self, spark):
         self.spark = spark
 
-    def transform(self, country_path, target_path):
-        values = self.spark.read.option("header", True).options(delimiter=";").csv(country_path)
-        labels = ["origin_country_code", "origin_country"]
-        dim_appl_org_country_df = self.spark.createDataFrame(values, labels)
-        dim_appl_org_country_df.write.mode("overwrite").parquet(target_path)
+    def transform(self, immi_file_path, target_path):
+        immigration_df = self.spark.read.option("header", True).options(delimiter=",").csv(immi_file_path)
+
+        transformed_df = immigration_df.groupBy(col("Country").alias("country")).agg(
+            round(mean('AverageTemperature'), 2).alias("average_temperature"),\
+            round(mean("AverageTemperatureUncertainty"),2).alias("average_temperature_uncertainty")
+            ).dropna()\
+            .withColumn("temperature_id", monotonically_increasing_id()) \
+            .select(["temperature_id", "country", "average_temperature", "average_temperature_uncertainty"]) 
+        transformed_df.write.mode("overwrite").parquet(target_path)
+
