fa1859a0fadc5edaef31238eee798cd02666833f
Tuan-Ngoc Tran <ngocthoson1@gmail.com>
2023-11-21 13:30:04 +0700

    edit

--- dags/scripts/spark/Transformer.py
+++ dags/scripts/spark/Transformer.py
@@ -3,7 +3,7 @@ class Transformer:
         self.spark = spark
         
     def transform(self, input_path, output_path, table_name, query_path):
-        self.spark.read.option("header", "true").option("delimiter", ",").csv(input_path).createTempView(table_name)
+        self.spark.read.csv(input_path, header=True, sep=",",  mode="DROPMALFORMED").dropna().dropDuplicates().createTempView(table_name)
         with open(query_path, 'r') as file:
             sql_query = file.read()
         self.spark.sql(sql_query).write.parquet(output_path)
